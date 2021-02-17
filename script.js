13f27efbba06a388db5174ae226ff44af38df6a5
Nikita Putintsev <oficialsmashgame@gmail.com>
2021-02-18 00:12:39 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+
+var PlaceHolder = 1;
+let second = 2;
+const pi = 3.14;
+let man = {
+    name: 'Vasiliy',
+    age: 12,
+    isMarried: false
+};
+
+let array = ['orrange.jpeg', 2]
+
+console.log(array[0]);
+
+let a = 7;
+console.log (2 + man.age + a);
