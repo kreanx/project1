f76ebe714d3e524ba5d122b940efa3e31402e069
Nikita <oficialsmashgame@gmail.com>
2021-02-19 12:39:32 +0300

    Урок по циклам

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 'use strict';
 
-
-let money = prompt ("Введите ваш бюджет за месяц?", ''),
+let money = +prompt ("Введите ваш бюджет за месяц?", ''),
     time = prompt ("Введите дату в формате YYYY-MM-DD", '');
 
 let appData = {
@@ -13,16 +12,33 @@ let appData = {
     savings: false,
 };
 
-let a1 = prompt ("Введите обязательную статью расходов в этом месяце", ''),
-    a2 = prompt ("Во сколько обойдётся?", ''),
-    a3 = prompt ("Введите обязательную статью расходов в этом месяце", ''),
-    a4 = prompt ("Во сколько обойдётся?", '');
-    
-appData.expenses.a1=a2;
-appData.expenses.a3=a4;
-alert(appData.budget / 30)
+for (let i = 0; i < 2; i++) {
+        let a = prompt ("Введите обязательную статью расходов в этом месяце", ''),
+            b = prompt ("Во сколько обойдётся?", '');
+
+            if ((typeof(a)) === 'string' && (typeof(b))!= null
+            && a != '' && b !='' && a.length < 50){
+                console.log("done");
+
+            appData.expenses[a] = b;
+        }
+};
+
+appData.moneyPerDay = appData.budget /30;
 
 
+alert("Ежедневный бюджет:" + appData.moneyPerDay);
 
-
+if (appData.moneyPerDay < 100) {
+    console.log("Минимлаьный уровень достатка");
+}
+    else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+        console.log("Средний урвоень достатка");
+    }
+    else if (appData.moneyPerDay > 2000){
+        console.log("Высокий урвоень достатка");
+    }
+    else {
+        console.log("Fatal error");
+    }
 
